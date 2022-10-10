638dbe60181dc3ff45225f1320c71db774ca8bc3
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-10-10 12:10:17 +0530

    Updated

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
-const user = require('./routes/user');
-const car = require('./routes/car');
+const mongoose = require('mongoose')
+
+const user = require('./routes/user')
+const car = require('./routes/car')
+
 
 const app = express();
-const port = 8000;
+const port = 4000;
+
+const url = 'mongodb:/localhost/express'
+
+mongoose.connect(url,{useNewUrlParser: true})
+const con = mongoose.connection
+
+con.on("open", ()=>{
+    console.log('MongoDB Connected');
+})
 
 app.use(express.json());
-app.use('/users',user);
-app.use('/cars',car)
+app.use('/user',user)
+app.use('/car',car)
 
-
-app.listen(port, () => {
-    console.log(`App starting on ${port}`);
+app.listen(port,(req,res) => {
+    console.log(`Example app listening on port ${port}`);
 })
